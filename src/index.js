a64e4b45ed4e7c417f349b2991bc18065fae18df
Valmir Almeida <almeidavalmir76@gmail.com>
2022-02-20 22:54:50 -0300

    metodos get post delete ...

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-const { response } = require("express");
+const { response, request } = require("express");
 const express = require("express");
 
 const app = express();
 
-app.get("/", (require, response) => {
-  return response.json({ message: "Olá NodeJs" });
+app.get("/courses", (require, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+// Cria novo
+app.post("/courses", (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+// Atualiza
+app.put("/courses", (request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+// Atualização Parcial
+app.patch("/courses", (request, response) => {
+  return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+// Deleta
+app.delete("/courses", (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 4"]);
 });
 
 app.listen(3000);
