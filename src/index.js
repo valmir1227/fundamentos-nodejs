2b6b5789d1cfc8ced70bb154b3fdd6074d5058bd
Valmir Almeida <almeidavalmir76@gmail.com>
2022-02-21 23:21:37 -0300

    paramters types

--- src/index.js
+++ src/index.js
@@ -3,17 +3,25 @@ const express = require("express");
 
 const app = express();
 
+// Middleware
+
+app.use(express.json());
+
 app.get("/courses", (require, response) => {
+  const query = request.query;
+  console.log(query);
   return response.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
 // Cria novo
 app.post("/courses", (request, response) => {
-  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+  const body = request.body;
+  console.log(body);
 });
 // Atualiza
 app.put("/courses/:id", (request, response) => {
-  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+  const { id } = request.params;
+  console.log(params);
 });
 // Atualização Parcial
 app.patch("/courses/:id", (request, response) => {
@@ -21,7 +29,7 @@ app.patch("/courses/:id", (request, response) => {
 });
 // Deleta
 app.delete("/courses/:id", (request, response) => {
-  return response.json(["Curso 1", "Curso 2", "Curso 4"]);
+  return response.json(["Curso 6", "Curso 7", "Curso 4"]);
 });
 
 app.listen(3000);
